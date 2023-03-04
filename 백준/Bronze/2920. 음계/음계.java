0ea8d0ce78b0db6a7eade4c66effe6f5f832d85e
Yurim Ji <118409530+yooriim@users.noreply.github.com>
2023-03-04 22:54:33 +0900

    [Bronze II] Title: 음계, Time: 116 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String temp = br.readLine();
+      if(temp.equals("1 2 3 4 5 6 7 8"))
+         System.out.println("ascending");
+      else if(temp.equals("8 7 6 5 4 3 2 1"))
+         System.out.println("descending");
+      else 
+         System.out.println("mixed");           
+   }
+
+}
