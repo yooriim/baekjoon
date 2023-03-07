520ea16c1786d18ba60fa7ed2ea49541d1ba9f14
yooriim <jiielly0126@gmail.com>
2023-03-07 11:20:28 +0900

    2023-03-27 백준 2741

---
+++ src/com/bj/class1/P_2741.java
@@ -0,0 +1,21 @@
+package com.bj.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2741 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=1;i<n+1;i++) {
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
