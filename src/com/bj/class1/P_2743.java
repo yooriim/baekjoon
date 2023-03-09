8a113561e135fbc2161aa005663f66d43e1373d6
yooriim <jiielly0126@gmail.com>
2023-03-09 17:01:25 +0900

    2023-03-09 백준 2743

---
+++ src/com/bj/class1/P_2743.java
@@ -0,0 +1,17 @@
+package com.bj.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P_2743 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(s.length());
+
+	}
+
+}
